c1acdefa3eed6bde5ea9cc1d6a2dcb1a76b3585d
Florence <lmj1996leo@gmail.com>
2023-02-28 09:53:59 +0800

    layer and folder update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,8 @@
   <h1>Florencelee</h1>
   <p>Nice to see ya! I think I will write this blog until i cant work...</p>
   <p>I'm Florencelee,you can call me “杠”, my fickname, because that's my friends gived.</p>
-  <p>I have graduate from a unfamous school- Huanghuaiuniversity, two years later,I teach myself knowledge about the development, until 200days later i find a job.
+  <p>I have graduate from a unfamous school- Huanghuaiuniversity, two years later,I teach myself knowledge about the development, until undefined (maybe infinity) days later i find a job.
+    Who knows the future, the base what you interested is importent, I think JS and Css is the base,make design as designer,rather than use framework like a liner-worker.
     That's so hard for everyone,I am so lucky by condition of 2023.I think what Evan you said is right.</p>
   <p>I like AnthonyFu's qoute:'A ship in harbor is safe, but that is not what ships are built for.'</p>
   <p>I will use it as a blank paper,but it seems more fashional.</p>
